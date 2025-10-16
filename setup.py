d6dfae354967c149f91e68bbdbcb910d0d3da9e2
BlueGod01 <roboticsengineer01@gmail.com>
2025-10-16 17:16:58 +0530

    setup file and requirements file added

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+from typing import List
+hypen_e = '-e .'# this inside requirement.txt will trigger the setup.py file
+def find_requirements(folder_path:str)->List[str]:
+    requirements = []
+    with open(folder_path, mode='r') as req:
+        requirements = req.readlines()
+        requirements = [obj.replace("\n",'') for obj in requirements]
+        requirements=requirements.remove(hypen_e)
+    return requirements        
+
+setup( name = 'ML-Project',
+      version='1.0.0',
+      packages = find_packages(),
+      author= 'BlueGod',
+      author_email='roboticsengineer01@gmail.com',
+      install_requires = find_requirements('requirements.txt')
+)
