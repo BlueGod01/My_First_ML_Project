601aa0084e1780129bd12807d53acf06eb79cb67
BlueGod01 <roboticsengineer01@gmail.com>
2025-10-18 17:35:49 +0530

    Small changes in logger.py and exception.py file.

--- src/exception.py
+++ src/exception.py
@@ -1,2 +1,28 @@
 '''this file is especially used for handling exceptions through out the code. 
-This is done for project execution monitoring and resolve issues in execution'''
+This is done for project execution monitoring and resolve issues in execution
+Whenever we will use 'try' block  and raise custom exception throughout our code
+this code will help to identify Errors. This code will tell in which 
+python script this error has raised, in which Line no and what is the cause of the error.'''
+import sys
+
+def error_message_detail(error,error_detail:sys):
+    _,_,exc_tab = error_detail.exc_info()#extracting the details about the error i.e file_name, line no etc
+    filename = exc_tab.tb_frame.f_code.co_filename #python script where the error has occured
+    error_message = "Error has occured in python script name [{0}] line number [{1}] error message [{2}]".format(
+        filename, exc_tab.tb_lineno, str(error)
+    )
+    return error_message
+
+
+class CustomException(Exception):
+    def __init__(self,error_message,error_detail:sys):
+        super().__init__(error_message)
+        self.error_message=error_message_detail(error_message,error_detail=error_detail)
+    
+    def __str__(self):
+        #this function is for printing error message when we perform print(Error)
+        return self.error_message
+    
+
+
+        
--- src/logger.py
+++ src/logger.py
@@ -1,2 +1,18 @@
 '''this script is specifically used for logging program 
-execution events especially debugs and critical execution'''
+execution events especially info, warning, error, critical execution'''
+import logging
+import os
+from datetime import datetime
+
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path=os.path.join(os.getcwd(),"logs")
+os.makedirs(logs_path,exist_ok=True)
+
+LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+)
+
